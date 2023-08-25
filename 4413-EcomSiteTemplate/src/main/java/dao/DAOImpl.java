d3049911c1f1851480053fb4e05b333e918704a9
MiaoNYA <gljgdxw@gmail.com>
2023-08-25 14:30:53 -0400

    finish allthe daoimpl task

--- 4413-EcomSiteTemplate/src/main/java/dao/DAOImpl.java
+++ 4413-EcomSiteTemplate/src/main/java/dao/DAOImpl.java
@@ -82,13 +82,85 @@ public class DAOImpl implements DAO {
 	@Override
 	public List<Item> findAllTops() {
 		// TODO Auto-generated method stub
-		return null;
+		List<Item> result = new ArrayList<Item>();
+
+		// join 3 tables to get needed info
+		String sql = "SELECT * FROM Item WHERE category = 'Shirts'";
+
+		Connection connection = null;
+		try {
+			connection = getConnection();
+			PreparedStatement statement = connection.prepareStatement(sql);
+			ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				
+				ProductOrderItem prod = new ProductOrderItem();
+				Item item = new Item();
+				
+				prod.setItemID(resultSet.getString("productID"));
+				
+				item.setProductID(resultSet.getString("productID"));
+				item.setProductName(resultSet.getString("productName"));
+				item.setColour(resultSet.getString("colour"));
+				item.setCategory(resultSet.getString("category"));
+				item.setBrand(resultSet.getString("category"));
+				item.setQuantity(resultSet.getInt("category"));
+				item.setPrice(resultSet.getDouble("price"));
+				item.setImage(resultSet.getString("image"));
+				
+				
+				result.add(item);
+				
+				
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			closeConnection(connection);
+		}
+		return result;
 	}
 
 	@Override
 	public List<Item> findAllBottoms() {
 		// TODO Auto-generated method stub
-		return null;
+		List<Item> result = new ArrayList<Item>();
+
+		// join 3 tables to get needed info
+		String sql = "SELECT * FROM Item WHERE category = 'Pants'";
+
+		Connection connection = null;
+		try {
+			connection = getConnection();
+			PreparedStatement statement = connection.prepareStatement(sql);
+			ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				
+				ProductOrderItem prod = new ProductOrderItem();
+				Item item = new Item();
+				
+				prod.setItemID(resultSet.getString("productID"));
+				
+				item.setProductID(resultSet.getString("productID"));
+				item.setProductName(resultSet.getString("productName"));
+				item.setColour(resultSet.getString("colour"));
+				item.setCategory(resultSet.getString("category"));
+				item.setBrand(resultSet.getString("category"));
+				item.setQuantity(resultSet.getInt("category"));
+				item.setPrice(resultSet.getDouble("price"));
+				item.setImage(resultSet.getString("image"));
+				
+				
+				result.add(item);
+				
+				
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			closeConnection(connection);
+		}
+		return result;
 	}
 
 	@Override
@@ -135,26 +207,159 @@ public class DAOImpl implements DAO {
 
 	@Override
 	public List<Item> searchKeyword(String keyWord) {
-		// TODO Auto-generated method stub
-		return null;
+	    List<Item> result = new ArrayList<>();
+
+	    // SQL query to search for the keyword in relevant columns
+	    String sql = "SELECT * FROM Item " +
+	                 "WHERE productName LIKE ? OR " +
+	                 "colour LIKE ? OR " +
+	                 "category LIKE ? OR " +
+	                 "brand LIKE ?";
+
+	    Connection connection = null;
+	    try {
+	        connection = getConnection();
+
+	        PreparedStatement statement = connection.prepareStatement(sql);
+
+	        String keywordPattern = "%" + keyWord + "%";
+	        statement.setString(1, keywordPattern); // productName
+	        statement.setString(2, keywordPattern); // colour
+	        statement.setString(3, keywordPattern); // category
+	        statement.setString(4, keywordPattern); // brand
+
+	        ResultSet resultSet = statement.executeQuery();
+
+	        while (resultSet.next()) {
+	            Item item = new Item();
+	            item.setProductID(resultSet.getString("productID"));
+	            item.setProductName(resultSet.getString("productName"));
+	            item.setColour(resultSet.getString("colour"));
+	            item.setCategory(resultSet.getString("category"));
+	            item.setBrand(resultSet.getString("brand"));
+	            item.setQuantity(resultSet.getInt("quantity"));
+	            item.setPrice(resultSet.getDouble("price"));
+	            item.setImage(resultSet.getString("image"));
+
+	            result.add(item);
+	        }
+	    } catch (SQLException ex) {
+	        ex.printStackTrace(); 
+	    } finally {
+	        closeConnection(connection); 
+	    }
+
+	    return result;
 	}
+
 
 	@Override
 	public List<Item> getSortHiLo(boolean ascending) {
-		// TODO Auto-generated method stub
-		return null;
+	    List<Item> result = new ArrayList<>();
+
+	    // SQL query to sort items by price (high to low)
+	    String sql = "SELECT * FROM Item ORDER BY price " + (ascending ? "ASC" : "DESC");
+
+	    Connection connection = null;
+	    try {
+	        connection = getConnection(); 
+	        PreparedStatement statement = connection.prepareStatement(sql);
+	        ResultSet resultSet = statement.executeQuery(sql);
+
+	        while (resultSet.next()) {
+	            Item item = new Item();
+	            item.setProductID(resultSet.getString("productID"));
+	            item.setProductName(resultSet.getString("productName"));
+	            item.setColour(resultSet.getString("colour"));
+	            item.setCategory(resultSet.getString("category"));
+	            item.setBrand(resultSet.getString("brand"));
+	            item.setQuantity(resultSet.getInt("quantity"));
+	            item.setPrice(resultSet.getDouble("price"));
+	            item.setImage(resultSet.getString("image"));
+
+	            result.add(item);
+	        }
+	    } catch (SQLException ex) {
+	        ex.printStackTrace(); 
+	    } finally {
+	        closeConnection(connection); 
+	    }
+
+	    return result;
 	}
 
 	@Override
-	public List<Item> getSortLoHi(boolean decesending) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Item> getSortLoHi(boolean descending) {
+	    List<Item> result = new ArrayList<>();
+
+	    // SQL query to sort items by price in ascending order (low to high)
+	    String sql = "SELECT * FROM Item ORDER BY price " + (descending ? "DESC" : "ASC");
+
+	    Connection connection = null;
+	    try {
+	        connection = getConnection(); // Replace with your connection logic
+	        PreparedStatement statement = connection.prepareStatement(sql);
+	        ResultSet resultSet = statement.executeQuery(sql);
+
+	        while (resultSet.next()) {
+	            Item item = new Item();
+	            // Populate the Item object with data from the result set
+	            item.setProductID(resultSet.getString("productID"));
+	            item.setProductName(resultSet.getString("productName"));
+	            item.setColour(resultSet.getString("colour"));
+	            item.setCategory(resultSet.getString("category"));
+	            item.setBrand(resultSet.getString("brand"));
+	            item.setQuantity(resultSet.getInt("quantity"));
+	            item.setPrice(resultSet.getDouble("price"));
+	            item.setImage(resultSet.getString("image"));
+
+	            result.add(item);
+	        }
+	    } catch (SQLException ex) {
+	        ex.printStackTrace(); // Handle or log the exception as needed
+	    } finally {
+	        closeConnection(connection); // Replace with your connection management
+	    }
+
+	    return result;
 	}
+
 
 	@Override
 	public List<Item> getSortAZ(boolean ascending) {
-		// TODO Auto-generated method stub
-		return null;
+	    List<Item> result = new ArrayList<>();
+
+	    // SQL query to sort items by product name (A to Z)
+	    String sql = "SELECT * FROM Item ORDER BY productName " + (ascending ? "ASC" : "DESC");
+
+	    Connection connection = null;
+	    try {
+	        connection = getConnection(); // Replace with your connection logic
+	        PreparedStatement statement = connection.prepareStatement(sql);
+	        ResultSet resultSet = statement.executeQuery(sql);
+
+	        while (resultSet.next()) {
+	            Item item = new Item();
+	            // Populate the Item object with data from the result set
+	            item.setProductID(resultSet.getString("productID"));
+	            item.setProductName(resultSet.getString("productName"));
+	            item.setColour(resultSet.getString("colour"));
+	            item.setCategory(resultSet.getString("category"));
+	            item.setBrand(resultSet.getString("brand"));
+	            item.setQuantity(resultSet.getInt("quantity"));
+	            item.setPrice(resultSet.getDouble("price"));
+	            item.setImage(resultSet.getString("image"));
+
+	            result.add(item);
+	        }
+	    } catch (SQLException ex) {
+	        ex.printStackTrace(); 
+	    } finally {
+	        closeConnection(connection); 
+	    }
+
+	    return result;
 	}
+
 
 }
